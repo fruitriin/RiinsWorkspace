10107874b3659e4b80d8d9c26e0a5719b62a92f5
fruitriin <nassii74@gmail.com>
2020-05-31 18:43:00 +0900

    feat: sample

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,32 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        Riin-sWorkspace
-      </h1>
-      <h2 class="subtitle">
-        My Blog
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div>
+    <div v-for="item in items" :key="item.id">
+      <p>{{ item.id }}</p>
+      <p>{{ item.tags }}</p>
+      <p v-html="item.body" />
+
+      <pre>{{ item }}</pre>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  async asyncData({ $axios }) {
+    const { data } = await $axios.get(
+      'https://riins-workspace.microcms.io/api/v1/articles',
+      {
+        headers: { 'X-API-KEY': process.env.API_KEY }
+      }
+    )
+    return {
+      items: data.contents
+    }
+  },
+  data() {
+    return {
+      items: []
+    }
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
