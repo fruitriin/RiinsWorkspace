9e618a402fd006313166366aad9be2d6c2295789
fruitriin <nassii74@gmail.com>
2020-08-08 23:59:30 +0900

    タイトルロゴを入れる

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <h1>
+      <nuxt-link to="/">
+        <img class="logo" src="~/assets/img/TitleLogo.png" />
+      </nuxt-link>
+    </h1>
     <nuxt />
   </div>
 </template>
@@ -24,32 +29,8 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  padding: 10px 30px;
-  color: #3b8070;
-  text-decoration: none;
-  border: 1px solid #3b8070;
-  border-radius: 4px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  padding: 10px 30px;
-  margin-left: 15px;
-  color: #35495e;
-  text-decoration: none;
-  border: 1px solid #35495e;
-  border-radius: 4px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.logo {
+  width: auto;
+  height: 60px;
 }
 </style>
